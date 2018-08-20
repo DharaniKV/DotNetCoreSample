2568a75e676b30fe35e9cdf13c649f0deec3e518
Dharani <dharani.kopparapu@CAPGEMINDEVSRV>
2018-08-20 12:56:54 +0300

    adding .netcore sample solution

--- DotNetCoreSample/Program.cs
+++ DotNetCoreSample/Program.cs
@@ -15,6 +15,7 @@ namespace DotNetCoreSample
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
+
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
